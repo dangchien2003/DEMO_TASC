6178e5085ae7d13c082e38357e89eb5480fafe51
dangchien2003 <chienboy03@gmail.com>
2024-10-15 23:45:47 +0700

    done

--- Java_Core_Basic/static_and_final/src/main/java/org/example/Main.java
+++ Java_Core_Basic/static_and_final/src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
 public class Main {
+    static class Vehicle {
+        String name;
+        int numWheel;
+    }
+
+    static {
+        count = 10;
+        System.out.println("Khởi tạo count = 10");
+    }
+
+    static int count;
+
     public static void main(String[] args) {
-        
+        System.out.println("start main");
+//        up(); lỗi vì phương thức up không phải là static
+        down();
+        System.out.println("count: " + count);
+        System.out.println("count in Main class: " + Main.count);
+    }
+
+    private void up() {
+        count += 1;
+        down();
+    }
+
+    private static void down() {
+        count -= 1;
     }
 }
+
